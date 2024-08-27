ccbfd0177f268ef641f83c85c1767ff958ee8e27
Gabriel Patrício <gabrieltp087@gmail.com>
2024-08-27 15:00:22 -0300

    Finishing pages

---
+++ src/pages/app/dashboard/dashboard.tsx
@@ -0,0 +1,31 @@
+import { Helmet } from 'react-helmet-async'
+
+import { DayOrdersAmountCard } from './day-orders-amount-card copy'
+import { MonthCanceledOrdersCard } from './month-canceled-orders-card copy'
+import { MonthOrdersAmountCard } from './month-orders-amount-card'
+import { MonthRevenueCard } from './month-revenue-card'
+import { PopularProductsChart } from './popular-products-chart'
+import { RevenueChart } from './revenue-chart'
+
+export function Dashboard() {
+  return (
+    <>
+      <Helmet title="Dashboard" />
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
+
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4">
+          <MonthRevenueCard />
+          <MonthOrdersAmountCard />
+          <DayOrdersAmountCard />
+          <MonthCanceledOrdersCard />
+        </div>
+
+        <div className="grid grid-cols-9 gap-4">
+          <RevenueChart />
+          <PopularProductsChart />
+        </div>
+      </div>
+    </>
+  )
+}
